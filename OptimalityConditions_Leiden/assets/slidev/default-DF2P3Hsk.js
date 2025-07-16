54d4e24e88c8b3fa79903080e6a4c70791381daf
Daniel Brosch <73886037+DanielBrosch@users.noreply.github.com>
2025-07-16 11:07:44 +0200

    fix paths?

---
+++ OptimalityConditions_Leiden/assets/slidev/default-DF2P3Hsk.js
@@ -0,0 +1,1 @@
+import{f as o,o as t,j as s}from"../modules/vue-DXfvL88u.js";import{_ as r}from"../index-CR-GP4hW.js";const c={},a={class:"slidev-layout default"};function n(e,l){return t(),o("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
